754e55bb8b36a5a2fc56bd5c9320289112a1561f
Tissa Khosla <tissa.music@gmail.com>
2019-11-13 14:41:07 -0500

    update seems to work next is delete

--- mainController.js
+++ mainController.js
@@ -10,15 +10,27 @@ const mainController = {
       .then(programsObjsArray => res.json(programsObjsArray))
   },
 
-  orchestra: function (req, res) { //r
-    Program.findOne({ orchestra: req.params.orchestra })
+  lookUpByOrchestra: function (req, res) { //r
+    Program.find({ orchestra: req.params.orchestra })
+      .then(programs => res.json(programs))
+  },
+
+  lookUpById: function (req, res) { //r
+    Program.findOne({ _id: req.params._id })
       .then(programs => res.json(programs))
   },
 
   new: function(req, res) { //c
     Program.create(req.body)
       .then(performance => res.json(performance))
-  }
+  },
+
+  editById: (req, res) => { //u
+    Program.findOneAndUpdate({_id: req.params._id}, req.body, { new: true, useFindAndModify: false })
+      .then(program => {
+        res.json(program)
+      })
+    }
 }
 
 module.exports = mainController
--- routes/route1974.js
+++ routes/route1974.js
@@ -4,7 +4,9 @@ const mainController = require('../mainController')
 
 router.get("/", mainController.redirect);
 router.get("/programs", mainController.index); //r
-router.get("/programs/orchestra/:orchestra", mainController.orchestra) //r
+router.get("/programs/orchestra/:orchestra", mainController.lookUpByOrchestra) //r
+router.get("/programs/_id/:_id", mainController.lookUpById) //r
 router.post("/programs", mainController.new) //c
+router.put("/programs/_id/:_id", mainController.editById) //u
 
 module.exports = router
