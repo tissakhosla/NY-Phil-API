73adad9a5353cce6dd81433cc393c9f85679d13d
Tissa Khosla <tissa.music@gmail.com>
2019-11-15 11:14:57 -0500

    added useUnifiedTopology: True

--- db/connection.js
+++ db/connection.js
@@ -9,10 +9,9 @@ if (process.env.NODE_ENV === "production") {
 
 // connect to the database, with the imported mongoose instance
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(instance =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
   .catch(error => console.log("Connection failed!", error));
 module.exports = mongoose;
-
