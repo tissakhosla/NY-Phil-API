84170954d49d6401aa3ccb83e4e8af423ace1f9e
Tissa Khosla <tissa.music@gmail.com>
2019-11-15 14:33:51 -0500

    lookup by composer route/query works in mongo  - comma seperated first and last name is not cool for params in the URL  - then I will reseed and push to heroku

--- controllers/programController.js
+++ controllers/programController.js
@@ -1,57 +1,57 @@
-  const Program = require('../models/Program');
+const Program = require('../models/Program');
 
-  const programController = {
-    redirect: function (req, res) { //c
-      res.redirect("/program")
-    },
+const programController = {
+  redirect: function (req, res) { //c
+    res.redirect("/program")
+  },
 
-    index: function (req, res) { //r
-      Program.find({})
-        .then(programsObjsArray => res.json(programsObjsArray))
-    },
+  index: function (req, res) { //r
+    Program.find({})
+      .then(programsObjsArray => res.json(programsObjsArray))
+  },
 
-    lookUpByOrchestra: function (req, res) { //r
-      Program.find({ orchestra: req.params.orchestra })
-        .then(programs => res.json(programs))
-    },
+  lookUpByComposer: function (req, res) {
+    Program.find({ works: { $elemMatch: { composer: req.params.composer } } })
+      .then(program => res.json(program))
+  },
 
-    lookUpById: function (req, res) { //r
-      Program.findOne({ _id: req.params._id })
-        .then(programs => res.json(programs))
-    },
+  lookUpByOrchestra: function (req, res) { //r
+    Program.find({ orchestra: req.params.orchestra })
+      .then(programs => res.json(programs))
+  },
 
-    new: function (req, res) { //c
-      Program.create(req.body)
-        .then(performance => res.json(performance))
-    },
+  lookUpById: function (req, res) { //r
+    Program.findOne({ _id: req.params._id })
+      .then(programs => res.json(programs))
+  },
 
-    editById: function (req, res) { //u
-      Program.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true, useFindAndModify: false })
-        .then(program => {
-          res.json(program)
-        })
-    },
+  new: function (req, res) { //c
+    Program.create(req.body)
+      .then(performance => res.json(performance))
+  },
 
-    deleteById: function (req, res) { //d
-      Program.findOneAndDelete({ _id: req.params._id }, { useFindAndModify: false })
-        .then(program => {
-          res.json(program)
-        })
-    },
+  editById: function (req, res) { //u
+    Program.findOneAndUpdate({ _id: req.params._id }, req.body, { new: true, useFindAndModify: false })
+      .then(program => {
+        res.json(program)
+      })
+  },
 
-    lookUpWorkById: function (req, res) {
-      Program.find({ works: { _id: req.params._id }})
-        .then(program => res.json(program))
-    }
+  deleteById: function (req, res) { //d
+    Program.findOneAndDelete({ _id: req.params._id }, { useFindAndModify: false })
+      .then(program => {
+        res.json(program)
+      })
   }
+}
 
-  // const innerController = {
-  //   lookUpComposer: function(req, res) {
-  //     Program.find({
-  //       "works": {composer: req.params.composer}})
-  //       .then(program => res.json(program))
-  //   }
-  // }
+// const innerController = {
+//   lookUpComposer: function(req, res) {
+//     Program.find({
+//       "works": {composer: req.params.composer}})
+//       .then(program => res.json(program))
+//   }
+// }
 
-  module.exports = programController
+module.exports = programController
   // module.exports = innerController
--- routes/programRoute.js
+++ routes/programRoute.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express()
 const programController = require('../controllers/programController')
-const innerController = require('../controllers/programController')
+// const innerController = require('../controllers/programController')
 
 
 router.get("/", programController.index); //r
+router.get("/composer/:composer", programController.lookUpByComposer) //r
 router.get("/orchestra/:orchestra", programController.lookUpByOrchestra) //r
 router.get("/id/:_id", programController.lookUpById) //r
 router.post("/", programController.new) //c
 router.put("/id/:_id", programController.editById) //u
 router.delete("/id/:_id", programController.deleteById) //d
-
-router.get("/works/id/:_id", programController.lookUpWorkById)
 
 module.exports = router
