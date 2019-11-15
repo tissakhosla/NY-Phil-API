4827f2d18d896e9c09e0678f02eb8f6f78bb0d63
Tissa Khosla <tissa.music@gmail.com>
2019-11-15 13:21:21 -0500

    installed cors

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 
 const parser = require('body-parser')
 
+app.use(cors())
 app.use(parser.json())
+
 app.use(require('./routes/routeIndex'))
 
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
-  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+  console.log(`✅Cors enabled server listening on PORT: ${app.get("port")} 🌟`);
 });
